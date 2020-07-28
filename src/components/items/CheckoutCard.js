e7e2dbbfe57bda4f537d3031b67e65e5dbcb59bb
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-07-28 09:22:53 -0500

    got owner information to display in checklout card

--- src/components/items/CheckoutCard.js
+++ src/components/items/CheckoutCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import CheckoutList from "./CheckoutList";
 import ApiManager from '../../modules/ApiManager';
 import React, { useState, useEffect } from 'react';
@@ -7,20 +7,25 @@ import React, { useState, useEffect } from 'react';
 
 const CheckoutCard = (props) => {
     const [owner, setOwner] = useState([]);
-     ApiManager.getOne("users", props.checkout.userId).then(response => {
-         setOwner(response)
-     })
-    
+    const getOwner = () => {
+        ApiManager.getOne("users", props.checkout.item.userId).then(response => {
+            setOwner(response)
+        })
+    }
+    useEffect(() => {
+        getOwner();    
+    }, []);
+
     return (
 
         <div className="card">
             <div className="item--card">
-                <h2 className="item--name">{props.checkout.item.id}</h2>
+                <h2 className="item--name">{props.checkout.item.name}</h2>
                 <p>By: {props.checkout.item.author}</p>
-                <p>Owner: {props.checkout.userId}</p>
+                <p>Owner: {owner.username}</p>
                 {/* {console.log("checkouts", props.checkouts.id)} */}
                 {/* <p>{props.dueDate}</p> */}
-                
+
 
             </div>
         </div>
