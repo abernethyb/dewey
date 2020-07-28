2f34e7995d78dcbd275bfccbc106b1e0de2e73d8
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-07-28 11:30:34 -0500

    made checkout object for later posting in item card

--- src/components/items/ItemCard.js
+++ src/components/items/ItemCard.js
@@ -6,21 +6,28 @@ const ItemCard = (props) => {
 
     // "userId": 5,
     // "itemId": 4,
-    // "checkoutDate": 1592179200,
-                       
+    // "checkoutDate": 1592179200,              
     // "dueDate": 1594929662,
     // "checkinDate": null,
     // "checkedOut": true,
     // "id": 1
 
     let milDate = Date.now()
-    console.log(milDate)
+    //console.log(milDate)
     let secDate = Math.round(milDate / 1000)
-    console.log(secDate)
+    // console.log(secDate)
+    let dueDate = secDate + 2419200 
+    //console.log(dueDate)
 
     const checkout = {
         userId: props.item.user.id,
-        itemId: props.item.id
+        itemId: props.item.id,
+        checkoutDate: secDate,
+        dueDate: dueDate,
+        checkinDate: null,
+        checkedOut: true,
+
+
     };
 
 
