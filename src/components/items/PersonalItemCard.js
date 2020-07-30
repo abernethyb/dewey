de432ddf40e83feebdc6ff5539e15abf410cdcc6
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-07-30 17:59:08 -0500

    denoted more clearly unavailable items in personal library

--- src/components/items/PersonalItemCard.js
+++ src/components/items/PersonalItemCard.js
@@ -5,16 +5,30 @@ import "./Item.css"
 
 const PersonalItemCard = (props) => {
 
-
+    let description = null
+    switch (props.item.categoryId) {
+        case 1:
+            description = props.item.otherInfo;
+            break;
+        case 2:
+            description = `By ${props.item.author}`;
+            break;
+        case 3:
+            description = `${props.item.makeOrPublisher}, ${props.item.year}`;
+            break;
+        default:
+            description = "";
+    }
 
 
     return (
 
         <div className="card">
+            {props.item.available ?
             <div className="personal--card">
                 <h2 className="item--name">{props.item.name}</h2>
-                <p>By: {props.item.author}</p>
-                <p>{props.item.available ? 'available' : 'unavailable'}</p>
+                <p>{description}</p>
+                <h3>{props.item.available ? 'Available' : 'Unavailable'}</h3>
                 <button type="button"
                     onClick={() => props.history.push(`/PersonalLibrary/${props.item.id}/edit`)}>
                     Edit
@@ -27,6 +41,26 @@ const PersonalItemCard = (props) => {
                 </button>
 
             </div>
+            :
+            <div className="personal--card--unavailable">
+            <h2 className="item--name">{props.item.name}</h2>
+            <p>{description}</p>
+            <h3>{props.item.available ? 'Available' : 'Unavailable'}</h3>
+            <button type="button"
+                disabled
+                onClick={() => props.history.push(`/PersonalLibrary/${props.item.id}/edit`)}>
+                Edit
+            </button>
+            <button
+                type="button"
+                className="card--button"
+                disabled
+                onClick={() => props.deleteItem(props.item.id)}>
+                Delete
+            </button>
+
+        </div>
+            }
         </div>
     );
 };
