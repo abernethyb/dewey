45a690d696ae6d08ad83f305f72b7ec0536f211d
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-08-03 16:14:05 -0500

    returned items no longer display in currently borrowing section

--- src/components/items/CheckoutList.js
+++ src/components/items/CheckoutList.js
@@ -33,12 +33,13 @@ const CheckoutList = (props) => {
         )
     }
 // && checkout.checkedOut &&
+// && checkout.checkinDate &&
   
     return (
         <>
             <div className="item--list">
             <h1 className="library--title">Items You're Currently Borrowing</h1>
-                {checkouts.map(checkout => checkout.userId === parseInt(sessionStorage.getItem("credentials")) && <CheckoutCard key={checkout.id} checkout={checkout} checkin={checkin} {...props} />)}
+                {checkouts.map(checkout => checkout.userId === parseInt(sessionStorage.getItem("credentials")) && checkout.checkinDate === "" && <CheckoutCard key={checkout.id} checkout={checkout} checkin={checkin} {...props} />)}
             </div>
         </>
     );
