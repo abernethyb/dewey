c367bcc31add3692f65fc864417a12c86d777562
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-08-05 11:13:10 -0500

    searchbar in public library is working

--- src/components/items/PublicLibrary.js
+++ src/components/items/PublicLibrary.js
@@ -9,6 +9,8 @@ import "./Library.css"
 
 const PublicLibrary = (props) => {
     const [items, setItems] = useState([]);
+    const [search, setSearch] = useState("")
+    const [filteredItems, setFilteredItems] = useState([])
 
     const getItems = () => {
         return ApiManager.getEmbeddedWithTwoExpand("items", "checkouts", "user", "category").then(itemsFromAPI => {
@@ -30,13 +32,27 @@ const PublicLibrary = (props) => {
         }
         )
     }
+    useEffect(() => {
+        setFilteredItems(
+            items.filter(item => {
+                return item.name.toLowerCase().includes(search.toLowerCase())
+            })
+        )
+    }, [search, items])
+
 
   
     return (
         <>
             <div className="item--list">
-                <h1 className="library--title">Public Library</h1>
-                {items.map(item => parseInt(sessionStorage.getItem("credentials")) !== item.user.id && <ItemCard key={item.id} item={item} postCheckout={postCheckout} {...props} />)}
+                <div className="library--title">
+                    <h1>Public Library</h1>
+                <div className="search">
+                    <input type="text" placeholder="Search by Item Name" onChange={event => setSearch(event.target.value)}></input>
+                    {console.log("filtered", filteredItems)}
+                </div>
+                </div>
+                {filteredItems.map(item => parseInt(sessionStorage.getItem("credentials")) !== item.user.id && <ItemCard key={item.id} item={item} postCheckout={postCheckout} {...props} />)}
             </div>
         </>
     );
